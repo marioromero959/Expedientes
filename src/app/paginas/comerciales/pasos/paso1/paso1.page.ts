2eff3aaf8271407f3cf21a0a1bec5010ff60918c
marioromero959 <marioromero959@gmail.com>
2021-09-02 16:42:24 -0300

    Funcionalidad paso1

--- src/app/paginas/comerciales/pasos/paso1/paso1.page.ts
+++ src/app/paginas/comerciales/pasos/paso1/paso1.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-paso1',
@@ -8,13 +9,38 @@ import { Router } from '@angular/router';
 })
 export class Paso1Page implements OnInit {
 
-  constructor(
-    private router: Router
-  ) { }
+  dataPaso1:FormGroup;
+  opcionSelec:string ='';
 
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+  ) {
+    this.dataPaso1 = this.fb.group({
+      CUIT:['hola',[Validators.required]],
+      CUENTA:['',[Validators.required]],
+      tipoPersona:['',[Validators.required]],
+    });
+    
+  }
   ngOnInit() {
   }
+
+  select(event){
+    this.opcionSelec = event.detail.value;
+    console.log(this.opcionSelec);
+  };
+
+
   terminarP1(){
-    this.router.navigate(['/comerciales/2'])
+    const datos = {
+      cuit: this.dataPaso1.value.CUIT,
+      cuenta: this.dataPaso1.value.CUENTA,
+      tipoP: this.dataPaso1.value.tipoPersona,
+      local:this.opcionSelec,
+    };
+    console.log(datos);
+    // this.router.navigate(['/comerciales/2'])
   }
+
 }
