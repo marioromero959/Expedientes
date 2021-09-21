ae14e0da8e50b5cea95f0a8984f3bcd82f01f141
marioromero959 <marioromero959@gmail.com>
2021-09-21 15:38:49 -0300

    Terminando condicion paso1

--- src/app/paginas/comerciales/pasos/paso1/paso1.page.ts
+++ src/app/paginas/comerciales/pasos/paso1/paso1.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 import { AlertController } from '@ionic/angular';
 import { FormulariosService } from 'src/app/servicios/datos/data-pasos/formularios.service';
+import { forEach } from '@angular-devkit/schematics';
 @Component({
   selector: 'app-paso1',
   templateUrl: './paso1.page.html',
@@ -13,21 +14,20 @@ export class Paso1Page implements OnInit {
   dataPaso1:FormGroup;
   opcionSelec:string ='';
   persona: string ="";
-  solic: string ="";
-  solicitudes:string[] = []
-
+  solic:any[]=[];
+  mostrar = true;
+  condicion = false;  
   public tipos = [
-    {val: 'Ampliación de rubro'},
-    {val: 'Apertura de sucursal'},
-    {val: 'Cambio de domicilio'},
-    {val: 'Cambio de Responsable'},
-    {val: 'Cambio de rubro'},
-    {val: 'Cese de Actividad'},
-    {val: 'Cierre Definitivo'},
-    {val: 'Rehabilitación'},
-    {val: 'Solicitud de Inscripción'}
+    {value: 'Ampliación de rubro'},
+    {value: 'Apertura de sucursal'},
+    {value: 'Cambio de domicilio'},
+    {value: 'Cambio de Responsable'},
+    {value: 'Cambio de rubro'},
+    {value: 'Cese de Actividad'},
+    {value: 'Rehabilitación'},
+    {value: 'Cierre Definitivo'},
+    {value: 'Solicitud de Inscripción'}
   ];
-
 
   constructor(
     private router: Router,
@@ -48,29 +48,30 @@ export class Paso1Page implements OnInit {
   };
 
   solicitud(event){
-    this.solic = event.detail.value;
-    if(this.solic === 'Solicitud de Inscripción'){
-      this.dataPaso1.get('cuenta').patchValue('');
-      this.dataPaso1.controls['cuenta'].disable(); 
+    if(event.detail.value == ''){
+      this.mostrar = true;
     }else{
-      console.log('otra');
+      this.mostrar = false;
     }
+    if(event.detail.value.includes("Solicitud de Inscripción")){
+        event.detail.value.splice(0,event.detail.value.length, "Solicitud de Inscripción");
+        this.dataPaso1.get('cuenta').patchValue('');
+        this.dataPaso1.controls['cuenta'].disable();
+        this.condicion = true;
+    }else if(event.detail.value.includes("Cierre Definitivo")){
+      event.detail.value.splice(0,event.detail.value.length, "Cierre Definitivo");
+      this.dataPaso1.controls['cuenta'].enable();
+      this.condicion = true;
+    }else{
+      this.dataPaso1.controls['cuenta'].enable();
+      this.condicion = false;
+    }
+
+
+    // console.log(event.detail.value);
+
+
   };
-
-  obtenerTipo(event){
-      if(event.detail.checked == true){
-        this.solicitudes.push(event.detail.value);
-        console.log('Arreglos de solic',this.solicitudes)
-      }else{
-          var i = this.solicitudes.indexOf(event.detail.value)
-          this.solicitudes.splice(i,1);
-          console.log('Arreglos de solic',this.solicitudes)
-
-      }
- 
-
-  }
-
 
   terminarP1(event){
     if (this.dataPaso1.valid) {
@@ -82,7 +83,7 @@ export class Paso1Page implements OnInit {
         solicitud:this.solic,
       };
       // cambiar if ---------
-      if(value.tipo == '' || value.local == '' || value.solicitud == ''){
+      if(value.tipo == '' || value.local == ''){
         this.presentAlert();
       }else{
         this.formData.mandar(value).subscribe();
