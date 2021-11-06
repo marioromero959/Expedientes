f1b2d43d97285910578215c9d4233df437ec2ed9
marioromero959 <marioromero959@gmail.com>
2021-11-06 02:47:32 -0300

    Agregando tablas

--- src/app/paginas/comerciales/habilitaciones/tablas/tablas.component.ts
+++ src/app/paginas/comerciales/habilitaciones/tablas/tablas.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
+import {MatTable} from '@angular/material/table';
 
 @Component({
   selector: 'app-tablas',
@@ -13,10 +14,40 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
     ]),
   ],
 })
-export class TablasComponent implements OnInit {
 
-  constructor() { }
+export class TablasComponent {
 
-  ngOnInit() {}
+  @ViewChild(MatTable) table: MatTable<any>;
+  datos = actividad;
+  dataSource = [...actividad];
+  columnasMostrar = ['fecha', 'description', 'position'];
+
+  expandirDatos:datosAct | null;
+
+  borrarAct(){
+  console.log('borrao')
+  this.dataSource.pop();
+  this.table.renderRows();
+  }
 
 }
+
+export interface datosAct {
+  position: string;
+  fecha: number;
+  description: string;
+}
+
+
+const actividad:any[]=[
+  {
+    position: 'Detalles',
+    fecha:1,
+    description:'blabla1',
+  },
+  {
+    position: 'Aaaa',
+    fecha:2,
+    description:'blabla2',
+  }
+]
