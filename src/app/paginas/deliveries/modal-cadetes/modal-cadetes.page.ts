1ae9967b692653eec35ea63661f12d7ea8961829
marioromero959 <marioromero959@gmail.com>
2021-11-19 00:10:16 -0300

    Terminando filtro cadetes

--- src/app/paginas/deliveries/modal-cadetes/modal-cadetes.page.ts
+++ src/app/paginas/deliveries/modal-cadetes/modal-cadetes.page.ts
@@ -8,40 +8,129 @@ import { FormControl } from '@angular/forms';
 })
 export class ModalCadetesPage implements OnInit {
 
-cadetes = [
-  {
-    nombre:'Juan',
-    tel:321654987,
-    descripcion:'Blablalba'
-  },
-  {
-    nombre:'Juanelo',
-    tel:321654987,
-    descripcion:'Blablalba'
-  },
-  {
-    nombre:'Roberto',
-    tel:321654987,
-    descripcion:'Blablalba'
-  },
-  {
-    nombre:'Maria',
-    tel:321654987,
-    descripcion:'Blablalba'
-  },
-  {
-    nombre:'Jimena',
-    tel:321654987,
-    descripcion:'Blablalba'
-  }
-]
-busquedaCadetes = new FormControl('');
+  cadetes = [
+    {
+      nombre:'Juan',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Juanelo',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Roberto',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Maria',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Mario',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Jime',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Juan',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Juanelo',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Roberto',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Maria',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Jimena',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Juan',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Juanelo',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Roberto',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Maria',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Jimena',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Juan',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Juanelo',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Roberto',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Maria',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+    {
+      nombre:'Jimena',
+      tel:321654987,
+      descripcion:'Blablalba'
+    },
+  ]
 
+busquedaCadetes = new FormControl('');
+pagina:number = 0;
 
   constructor(private modalCtrl: ModalController) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+  
+anterior(){
+  if(this.pagina > 0)
+  this.pagina -= 3;
+}
+siguiente(){
+  this.pagina += 3;
+}
+  
 cerrarModal(){
   this.modalCtrl.dismiss();
 }
--- src/app/paginas/deliveries/modal-cadetes/pipe/filtro-cadetes.pipe.ts
+++ src/app/paginas/deliveries/modal-cadetes/pipe/filtro-cadetes.pipe.ts
@@ -5,15 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FiltroCadetesPipe implements PipeTransform {
 
-  transform(value: any, arg:any): unknown {
-    const cadetesFiltrados = [];
-    for(const cadete of value){
-      if(cadete.nombre.toLowerCase().indexOf(arg.toLowerCase()) > -1){
-        cadetesFiltrados.push(cadete)
+  transform(value: any, busqueda:any,paginacion:number = 0): any {
+    if(busqueda === ''){
+      return value.slice(paginacion,paginacion+3)
+    }else{
+      const cadetesFiltrados = [];
+      for(const cadete of value){
+        if(cadete.nombre.toLowerCase().indexOf(busqueda.toLowerCase()) > -1){
+          cadetesFiltrados.push(cadete)
+        }
       }
+      return cadetesFiltrados.slice(paginacion,paginacion+3);
     }
-    return cadetesFiltrados;
     
-  }
+  } 
 
 }
